c49af8a1db6335ec4b2d2dff422f3330b9856a2f
Kristýna Švandová <kristyna.svandova@gmail.com>
2023-01-03 16:56:34 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
