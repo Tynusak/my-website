d16c7ecafd182478d9916212d908ec50452b3f0d
Kristýna Švandová <kristyna.svandova@gmail.com>
2023-02-07 17:50:35 +0100

    Navigation - static button

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import './Navigation.css';
 
 export const Navigation = () => {
-  return <div>Navigace</div>;
+  return (
+    <div className="navigation">
+      <button className="nav__button" id="nav__button">
+        <i class="fa-solid fa-bars"></i>
+      </button>
+    </div>
+  );
 };
