f98b4e11d9376b93a0264ee76985880f3573e23c
Kristýna Švandová <kristyna.svandova@gmail.com>
2023-02-09 18:07:29 +0100

    Dark mode button added

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import './ThemeSwitcher.css';
 
 export const ThemeSwitcher = () => {
-  return <div>Dark mode tlačítko</div>;
+  return (
+    <div className="dark__mode">
+      <label htmlFor="checkbox" className="theme__switcher">
+        <input type="checkbox" />
+        <div className="slider round"></div>
+      </label>
+    </div>
+  );
 };
