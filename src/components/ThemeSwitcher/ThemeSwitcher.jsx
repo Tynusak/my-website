6ce45b7c8eecb3cf28db86a3929eb2f712a4d1c0
Kristýna Švandová <kristyna.svandova@gmail.com>
2023-02-18 17:45:52 +0100

    Dark mode button deleted

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -2,12 +2,5 @@ import React from 'react';
 import './ThemeSwitcher.css';
 
 export const ThemeSwitcher = () => {
-  return (
-    <div className="dark__mode">
-      <label htmlFor="checkbox" className="theme__switcher">
-        <input type="checkbox" />
-        <div className="slider round"></div>
-      </label>
-    </div>
-  );
+  return <div className="dark__mode">Dark mode button</div>;
 };
