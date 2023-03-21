51c6257dd915093f0c40a63cfc45e60c3114302c
Kristýna Švandová <kristyna.svandova@gmail.com>
2023-03-21 12:01:23 +0100

    link test

--- src/pages/CVPage.jsx
+++ src/pages/CVPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Header } from '../components/Header/Header';
 import { Main } from '../components/Main/Main';
 import { H1 } from '../components/headlines/H1/H1';
-import { H2 } from '../components/headlines/H2/H2';
+//import { H2 } from '../components/headlines/H2/H2';
 //import { H3 } from '../components/headlines/H3/H3';
 import { Footer } from '../components/Footer/Footer';
 
