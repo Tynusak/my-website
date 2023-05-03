62baaaf3c18ab4551b758e8ef666817d893da7d9
Kristýna Švandová <kristyna.svandova@gmail.com>
2023-05-03 15:42:25 +0200

    Conversion-pages

--- src/pages/CVPage.js
+++ src/pages/CVPage.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Header } from '../components/Header/Header';
 import { Main } from '../components/Main/Main';
 import { H1 } from '../components/headlines/H1/H1';
@@ -14,7 +13,7 @@ export const CVPage = ({ switchTheme, theme }) => {
       <Main home={false}>
         <H1 text="Curricullum vitae" home={false} />
         <IntroSection>
-          <H2 text="O mně"/>
+          <H2 text="About"/>
         </IntroSection>
       </Main>
       <Footer />
