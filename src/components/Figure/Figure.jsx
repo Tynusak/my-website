483c3a78935ff79760211612902b6ca72d05327a
Kristýna Švandová <kristyna.svandova@gmail.com>
2023-05-03 15:00:05 +0200

    New components - IntroSection,Figure

---
+++ src/components/Figure/Figure.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Figure.css';
+
+export const Figure = ()=>{
+  return <figure>
+    <img src="" alt="" />
+    
+  </figure>
+}
---
+++ src/components/IntroSection/IntroSection.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './IntroSection.css';
+
+export const IntroSection = ({children}) => {
+  return <section className='intro'>{children}</section>;
+};
